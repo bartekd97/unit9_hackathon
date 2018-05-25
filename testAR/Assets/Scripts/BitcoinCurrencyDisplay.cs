e28575c5b64921dfaeef7433e5e84f1a1252fb2d
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 13:33:59 +0000

    dodany game manager w postaci statycznej klasy GameGlobal oraz spawnowanie i startowanie koparki z poziomu skryptu

---
+++ testAR/Assets/Scripts/BitcoinCurrencyDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitcoinCurrencyDisplay : MonoBehaviour {
+    private UnityEngine.UI.Text canvasText;
+	void Start () {
+        canvasText = GetComponent<UnityEngine.UI.Text>();
+    }
+	
+	void Update () {
+        if (GameGlobal.isGameStarted)
+            canvasText.text = "Zebrane bitcoiny: " + GameGlobal.bitcoinsCurrency.ToString();
+        else
+            canvasText.text = "";
+    }
+}
---
+++ testAR/Assets/Scripts/BitcoinMiner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitcoinMiner : MonoBehaviour {
+
+    public float MinerTick = 0.5f; // co ile sekund ma dodac bitcoiny
+    public int MinerAmount = 1; // ile ma bitcoinow dodawac
+
+    private float _time;
+	void Start () {
+        _time = Time.time;
+	}
+	void Update () {
+        if (!GameGlobal.isGameStarted)
+            return;
+
+        float dt = Time.time - _time;
+        if (dt >= MinerTick)
+        {
+            _time = Time.time;
+            GameGlobal.bitcoinsCurrency += MinerAmount;
+        }
+	}
+}
