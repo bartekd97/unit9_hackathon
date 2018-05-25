7e7c55c9aa6f74f4bca9f04df396598897d45695
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 22:28:32 +0000

    Lepsze UI i klikanie koparki Update prefaba koparki

---
+++ testAR/Assets/Scripts/MenuIconContainer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuIconContainer : MonoBehaviour {
+    public enum IconType
+    {
+        Active,
+        Disabled,
+        Hover
+    }
+    public Sprite activeIcon;
+    public Sprite disabledIcon;
+    public Sprite hoverIcon;
+    public UnityEngine.UI.Image image;
+
+    public Interactable interactable;
+
+	void Start () {
+        SetIconType(IconType.Active);
+	}
+	
+    public void SetIconType( IconType type )
+    {
+        switch (type)
+        {
+            case IconType.Active:
+                image.sprite = activeIcon;
+                break;
+            case IconType.Disabled:
+                image.sprite = disabledIcon;
+                break;
+            case IconType.Hover:
+                image.sprite = hoverIcon;
+                break;
+            default:
+                break;
+        }
+        if (interactable)
+        {
+            if (type == IconType.Disabled)
+                interactable.isInteractable = false;
+            else
+                interactable.isInteractable = true;
+        }
+    }
+}
