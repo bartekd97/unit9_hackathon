01d5259bc7cd22a2394c36365cfc297255b705c6
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 13:39:59 +0000

    healthsystem v1

---
+++ testAR/Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public float maxHealth;
+    public float currentHealth;
+	void Start () {
+        currentHealth = maxHealth;
+	}
+	
+    public void SubtractHealth(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0) Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
