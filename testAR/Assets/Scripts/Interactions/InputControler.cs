6adabd4943dfccddf8183363240d255bd2d8724e
bartosz <bartosz@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 17:13:59 +0000

    Tap management - basics

---
+++ testAR/Assets/Scripts/Interactions/InputControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+public class InputControler : MonoBehaviour {
+    
+    bool isCreated=false;
+	void Update () {
+        foreach(var touch in Input.touches)
+        {
+            Shoot(touch.position);
+        }
+        isCreated = false;
+	}
+
+    void Shoot(Vector2 screenPoint)
+    {
+        var ray = Camera.main.ScreenPointToRay(screenPoint);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray,out hitInfo)){
+            var script = hitInfo.transform.gameObject.GetComponent("Interactable");
+            if(script != null)
+            {
+                Type t = Type.GetType("Interactable");
+                MethodInfo method = t.GetMethod("OnTap");
+                method.Invoke(script, null);
+            }
+        }
+    }
+}
---
+++ testAR/Assets/Scripts/Menu/CaptureMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CaptureMenu : MonoBehaviour {
+
+    public enum Mode
+    {
+        switchScene = 0, switchMenu= 1, exit=2
+    }
+
+    public Mode mode;
+
+    [Tooltip("If none leave it")]
+    public GameObject NextMenu;
+    public GameObject Menu;
+
+    private void Start()
+    {
+        Menu = gameObject.transform.parent.gameObject;
+    }
+
+    [Tooltip("If none leave it")]
+    public string NextScene;
+
+    public void Capture()
+    {
+        switch(mode)
+        {
+            case Mode.switchScene:
+                SwitchScene();
+                break;
+            case Mode.switchMenu:
+                SwitchMenu();
+                break;
+            case Mode.exit:
+                Quit();
+                break;
+        }
+    }
+    
+    public void SwitchMenu()
+    {
+        Debug.Log(Menu.name);
+        Instantiate(NextMenu, gameObject.transform.parent.position, Quaternion.identity);
+        Destroy(Menu);
+    }
+
+    public void SwitchScene()
+    {
+        Debug.Log(NextScene);
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
