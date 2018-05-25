bc0fa9f6dbc4af6e80d6939cbbb882a6f3436aa6
bartlomiej <bartekd97@gmail.com>
2018-05-25 13:33:59 +0000

    dodany game manager w postaci statycznej klasy GameGlobal oraz spawnowanie i startowanie koparki z poziomu skryptu

---
+++ testAR/Assets/Scripts/GameGlobal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameGlobal {
+    #region zmienne
+    private static bool _isGameStarted = false;
+
+    public static bool isGameStarted
+    {
+        get
+        {
+            return _isGameStarted;
+        }
+    }
+    private static BitcoinMiner _bitcoinMiner;
+    public static BitcoinMiner bitcoinMiner
+    {
+        get
+        {
+            return _bitcoinMiner;
+        }
+    }
+
+    public static int bitcoinsCurrency = 0;
+    #endregion
+
+
+    public static void StartGame( GameObject miner, Transform parent, Vector3 minerPosition, Quaternion minerRotation )
+    {
+        if (_isGameStarted)
+            return;
+
+        _bitcoinMiner = (GameObject.Instantiate(miner, minerPosition, minerRotation, parent) as GameObject).GetComponent<BitcoinMiner>();
+        bitcoinsCurrency = 0;
+
+        _isGameStarted = true;
+    }
+}
---
+++ testAR/Assets/Scripts/MainScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+    public GameObject bitcoinMinerPrefab;
+
+	void Start () {
+        GameGlobal.StartGame(bitcoinMinerPrefab, transform, Vector3.forward*20, Quaternion.identity);
+    }
+}
