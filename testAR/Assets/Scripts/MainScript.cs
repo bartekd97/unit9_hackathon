f3d53103976936e1aed7c115435fdfd4aad81231
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 15:54:12 +0000

    Proste AI, poczatek dopiero

--- testAR/Assets/Scripts/MainScript.cs
+++ testAR/Assets/Scripts/MainScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class MainScript : MonoBehaviour {
     public GameObject bitcoinMinerPrefab;
+    public Transform robotPortal;
+    public GameObject robotPrefab;
 
 	void Start () {
         GameGlobal.StartGame(bitcoinMinerPrefab, transform, Vector3.forward*20, Quaternion.identity);
+        Instantiate(robotPrefab, robotPortal.position, robotPortal.rotation, robotPortal);
     }
 }
