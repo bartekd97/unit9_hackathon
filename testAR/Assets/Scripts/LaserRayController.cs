b513a3f920cda24bf22791210b612ed5e1d624c6
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 14:47:15 +0000

    better laser

--- testAR/Assets/Scripts/LaserRayController.cs
+++ testAR/Assets/Scripts/LaserRayController.cs
@@ -7,12 +7,35 @@ public class LaserRayController : MonoBehaviour {
     [HideInInspector]
     public float damage;
 
+    private bool enemyInRay;
+
+    private void Start()
+    {
+        enemyInRay = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Enemy")
         {
-            // Deal with enemy health script or whatever
-            other.gameObject.GetComponent<Health>().SubtractHealth(damage);
+            enemyInRay = true;
+            StartCoroutine(DealingDamage(other.gameObject.GetComponent<Health>()));
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy") enemyInRay = false;
+    }
+
+    IEnumerator DealingDamage(Health enemy)
+    {
+        while (enemyInRay)
+        {
+            enemy.SubtractHealth(damage);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        if (!enemyInRay) yield break;
+
     }
 }
