b5cb4d8aa76a7704e73dc5482341d0e91bab0338
bartlomiej <bartekd97@gmail.com>
2018-05-25 22:28:32 +0000

    Lepsze UI i klikanie koparki Update prefaba koparki

---
+++ testAR/Assets/Scripts/MenuTextIconUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuTextIconUpdater : MonoBehaviour {
+
+    public Text LevelAndMake;
+    public Text ExpToNext;
+    public MenuIconContainer LevelupIcon;
+
+	void Update () {
+        if (!GameGlobal.isGameStarted)
+            return;
+
+        LevelAndMake.text = "Level: 3\nMake: 3 BTC/min";
+        ExpToNext.text = "Exp to next lvl: 3000";
+        LevelupIcon.SetIconType(MenuIconContainer.IconType.Disabled);
+	}
+}
