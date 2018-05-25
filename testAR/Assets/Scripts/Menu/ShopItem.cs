2b64ef8b8a873857a6bb61ef13eb56d596298928
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-25 23:42:36 +0000

    otwiera sie shop w menu koparki

---
+++ testAR/Assets/Scripts/Menu/ShopItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour {
+
+    public string name;
+    public float price;
+    public Text nameText;
+    public Text priceText;
+    public MenuIconContainer icon;
+
+	void Update () {
+        nameText.text = name;
+        priceText.text = "$" + price.ToString();
+        if (price > 10000)
+            icon.SetIconType(MenuIconContainer.IconType.Disabled);
+        else
+            icon.SetIconType(MenuIconContainer.IconType.Active);
+	}
+}
