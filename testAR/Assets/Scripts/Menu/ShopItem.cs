754acddc19e8cef47f70011e981e543b49b009a8
piotr <piotr@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 14:48:59 +0000

    some bugs solved

--- testAR/Assets/Scripts/Menu/ShopItem.cs
+++ testAR/Assets/Scripts/Menu/ShopItem.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopItem : MonoBehaviour {
+public class ShopItem : MonoBehaviour
+{
 
     public string name;
     public float price;
     public Text nameText;
     public Text priceText;
     public MenuIconContainer icon;
+    public GameObject product;
     public GoogleARCore.Examples.HelloAR.HelloARController helloARController;
 
     private void Start()
@@ -17,9 +19,10 @@ public class ShopItem : MonoBehaviour {
         helloARController = GameObject.FindGameObjectWithTag("PlaceController").GetComponent<GoogleARCore.Examples.HelloAR.HelloARController>();
     }
 
-    void Update () {
+    void Update()
+    {
         //if (!GameGlobal.isGameStarted)
-          //  return;
+        //  return;
 
         nameText.text = name;
         priceText.text = "$" + price.ToString();
@@ -27,12 +30,18 @@ public class ShopItem : MonoBehaviour {
             icon.SetIconType(MenuIconContainer.IconType.Disabled);
         else
             icon.SetIconType(MenuIconContainer.IconType.Active);
-	}
+    }
 
     public void OnTouchBuy()
     {
-        Debug.Log( "Kupiono " + name );
+        if(product == null)
+        {
+            Debug.Log("Brak ustawionej rzeczy do zakupu");
+            return;
+        }
+        Debug.Log("Kupiono " + name);
         MenuOpenManager.CloseCurrent();
+        helloARController.SetPrefab(product);
         helloARController.placeMode = 4;
     }
 }
