463157dfc8ca3ea083a793a43edc5eb7b14a4186
bartosz <bartosz@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 02:25:47 +0000

    Debugging teleport

---
+++ testAR/Assets/Scripts/Debugging/DebugTeleporters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTeleporters : MonoBehaviour {
+
+    private GameObject plane;
+    
+    private void Update()
+    {
+        plane = gameObject.transform.GetChild(0).gameObject;
+        if (plane != null)
+        {
+            int i = 0;
+            foreach (Vector3 v3 in plane.GetComponent<GoogleARCore.Examples.Common.DetectedPlaneVisualizer>().m_MeshVertices)
+            {
+                Debug.Log(i +" "+ v3.x+" "+v3.y+" "+v3.z);
+                i++;
+            }
+            i = 0;
+        }
+    }
+}
