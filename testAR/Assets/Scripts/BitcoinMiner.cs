680c509a8a040b970d8d7296078e6a988e3408c9
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 23:38:10 +0000

    miner Particles

--- testAR/Assets/Scripts/BitcoinMiner.cs
+++ testAR/Assets/Scripts/BitcoinMiner.cs
@@ -6,9 +6,11 @@ public class BitcoinMiner : MonoBehaviour {
 
     public float MinerTick = 1f; // co ile sekund ma dodac bitcoiny
     public float MinerAmount = 0.01f; // ile ma bitcoinow dodawac
+    public GameObject sparksParticles;
 
     private float _time;
 	void Start () {
+        SetParticlesState(false);
         _time = Time.time;
 	}
 	void Update () {
@@ -22,7 +24,10 @@ public class BitcoinMiner : MonoBehaviour {
             GameGlobal.bitcoinsCurrency += MinerAmount;
         }
 	}
-
+    public void SetParticlesState(bool active)
+    {
+        sparksParticles.SetActive(active);
+    }
     public void LeveledUp()
     {
         if(gameObject.GetComponent<LevelController>().currentLevel > 2) MinerTick /= gameObject.GetComponent<LevelController>().currentLevel - 1;
--- testAR/Assets/Scripts/Health.cs
+++ testAR/Assets/Scripts/Health.cs
@@ -8,6 +8,7 @@ public class Health : MonoBehaviour {
     public float currentHealth;
     public bool baseObject;
 
+    private float deltaHP;
     private LevelController levelController;
 	void Start () {
         currentHealth = maxHealth;
@@ -39,5 +40,24 @@ public class Health : MonoBehaviour {
             //Show losing screen, reset game or something
         }
         else Destroy(gameObject);
+    }
+
+    IEnumerator CalculatingDeltaHP()
+    {
+        float tempHP = currentHealth;
+        yield return new WaitForSeconds(.3f);
+        deltaHP = currentHealth - tempHP;
+
+        if (baseObject)
+        {
+            if (deltaHP >= 0)
+            {
+                gameObject.GetComponent<BitcoinMiner>().SetParticlesState(false);
+            }
+            else
+            {
+                gameObject.GetComponent<BitcoinMiner>().SetParticlesState(false);
+            }
+        }
     }
 }
