3f78da1371ed580893a7f3746d75e135d9d3ccd0
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 10:44:09 +0000

    sentrygun timeout

--- testAR/Assets/Scripts/SentryGunController.cs
+++ testAR/Assets/Scripts/SentryGunController.cs
@@ -7,13 +7,16 @@ public class SentryGunController : MonoBehaviour {
     [Tooltip("Set this value negative to have laser turned on constantly")]
     public float shotDuration;
     public float laserDamage;
+    public float timeout;
 
     public Dictionary<string, bool> powerUps = new Dictionary<string, bool>();
     public Dictionary<string, bool> powerUpsUpgraded = new Dictionary<string, bool>();
     public Dictionary<string, float> powerUpsValues = new Dictionary<string, float>();
 
+    private bool canShoot;
     private GameObject laserRay;
 	void Start () {
+        canShoot = true;
         powerUps.Add("Freeze", false);
         powerUps.Add("SlowDown", false);
         powerUps.Add("Fire", false);
@@ -45,16 +48,31 @@ public class SentryGunController : MonoBehaviour {
         }
     }
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.X) && shotDuration > 0) Shoot();
+        if (Input.GetKeyDown(KeyCode.X) && shotDuration > 0 && canShoot) Shoot();
 	}
     public void Shoot()
     {
         StartCoroutine(LaserShot(shotDuration));
+    }
+
+    IEnumerator Timeout(float time)
+    {
+        canShoot = false;
+        
+        while(time > 0)
+        {
+            yield return new WaitForSeconds(0.2f);
+            time -= 0.2f;
+            // print reaming time to this crazy ui window
+        }
+
+        canShoot = true;
     }
     IEnumerator LaserShot(float duration)
     {
         laserRay.SetActive(true);
         yield return new WaitForSeconds(duration);
         laserRay.SetActive(false);
+        StartCoroutine(Timeout(timeout));
     }
 }
