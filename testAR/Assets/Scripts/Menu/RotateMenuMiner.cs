6a2b8f8901e29f5527dafb6a5033a8cab4fbb50f
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 16:15:45 +0000

    menu prefab

---
+++ testAR/Assets/Scripts/Menu/RotateMenuMiner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMenuMiner : MonoBehaviour {
+	void Update () {
+        transform.RotateAround(transform.position, transform.forward, 2f);
+	}
+}
