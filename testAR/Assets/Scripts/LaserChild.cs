94fe2cf5919cbdfdeebed0c017ce5e99780242a8
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 22:24:07 +0000

    double laser no dmg bug fixed

---
+++ testAR/Assets/Scripts/LaserChild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserChild : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.transform.parent.gameObject.GetComponent<LaserRayController>().OnChildTriggerEnter(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        gameObject.transform.parent.gameObject.GetComponent<LaserRayController>().OnChildTriggerExit(other);
+    }
+}
