835251e9e466ebfd5bc41f9b7c2efcb1773d8b2d
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 09:08:31 +0000

    shop system v1

--- testAR/Assets/Scripts/GameGlobal.cs
+++ testAR/Assets/Scripts/GameGlobal.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class GameGlobal {
     #region zmienne
     private static bool _isGameStarted = false;
-
+    private static BitcoinPrice bitcoinStock;
     public static bool isGameStarted
     {
         get
@@ -23,10 +23,50 @@ public static class GameGlobal {
     }
 
     public static int enemiesOnTheScene = -1;
-    public static int playerTokens = 0;
+    public static int playerTokens = 0; 
     public static float bitcoinsCurrency = 0;
-    public static float bitcointExchange;
+    public static float usdCurrency = 0;
+    public static float bitcoinExchange;
     #endregion
+    public static void AddCash(string currency, float amount)
+    {
+        bitcoinExchange = bitcoinStock.bitcoinPrice;
+        BTCToUSD(bitcoinExchange, bitcoinsCurrency);
+        if(currency.ToLower() == "usd" || currency.ToLower() == "dollars" || currency == "$")
+        {
+            usdCurrency += amount;
+            USDToBTC(bitcoinExchange, usdCurrency);
+        }else if(currency.ToLower() == "btc" || currency.ToLower() == "bitcoins" || currency.ToLower() == "bitcoin")
+        {
+            bitcoinsCurrency += amount;
+            BTCToUSD(bitcoinExchange, bitcoinsCurrency);
+        }
+    }
+
+    public static void SubtractCash(string currency, float amount)
+    {
+        BTCToUSD(bitcoinExchange, bitcoinsCurrency);
+        if (currency.ToLower() == "usd" || currency.ToLower() == "dollars" || currency == "$")
+        {
+            usdCurrency -= amount;
+            USDToBTC(bitcoinExchange, usdCurrency);
+        }
+        else if (currency.ToLower() == "btc" || currency.ToLower() == "bitcoins" || currency.ToLower() == "bitcoin")
+        {
+            bitcoinsCurrency -= amount;
+            BTCToUSD(bitcoinExchange, bitcoinsCurrency);
+        }
+    }
+
+    public static void USDToBTC(float btcExchange, float cash)
+    {
+        bitcoinsCurrency = cash / btcExchange;
+    }
+
+    public static void BTCToUSD(float btcExchange, float cash)
+    {
+        usdCurrency = bitcoinsCurrency * btcExchange;
+    }
 
     public static void CountEnemies()
     {
---
+++ testAR/Assets/Scripts/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+
+    public Dictionary<string, float> prices = new Dictionary<string, float>();
+	void Start () {
+        prices.Add("dupa", 50000);
+	}
+	
+    public void Buy(string product)
+    {
+        GameGlobal.SubtractCash("usd", prices[product]);
+        //do stuff with switch case here
+    }
+	void Update () {
+		
+	}
+}
