576800a21ba9a671dc788af660d174b82feffd28
bartlomiej <bartlomiej@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 12:06:34 +0000

    dodane indicatory nad dzialkami i poprawiony prefab

--- testAR/Assets/Scripts/Menu/MenuWindow.cs
+++ testAR/Assets/Scripts/Menu/MenuWindow.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MenuWindow : MonoBehaviour {
 
+    public bool isIndicator = false;
+    public MenuWindow[] indicatorMenus;
+
+    [HideInInspector]
+    public List<MenuWindow> indicatorCallList = new List<MenuWindow>();
     private const float C_EFFECT_SPEED = 3.0f;
     private const float C_DOWN_SLIDE = 0.25f;
 
@@ -13,10 +18,22 @@ public class MenuWindow : MonoBehaviour {
     private float fromLevel = 0f;
     private Vector3 originalPos;
     private Vector3 originalScale;
+    private int indicatorCount = 0;
 
     void Start () {
         UpdatePosition(transform.position);
         UpdateScale(transform.localScale);
+
+        if (isIndicator)
+        {
+            foreach (MenuWindow wnd in indicatorMenus)
+                wnd.AddIndicatorCall(this);
+            FadeIn();
+        }
+    }
+    public void AddIndicatorCall( MenuWindow ind )
+    {
+        indicatorCallList.Add(ind);
     }
 
     public void UpdatePosition( Vector3 pos )
@@ -38,7 +55,10 @@ public class MenuWindow : MonoBehaviour {
         currentLevel = Mathf.Max(0f, Mathf.Min(1f, currentLevel));
 
         transform.localScale = originalScale * currentLevel;
-        transform.position = originalPos - Vector3.up * C_DOWN_SLIDE * (1f - currentLevel);
+        if (isIndicator)
+            transform.position = originalPos + Vector3.up * C_DOWN_SLIDE * (1f - currentLevel);
+        else
+            transform.position = originalPos - Vector3.up * C_DOWN_SLIDE * (1f - currentLevel);
 
         if (currentLevel == 0 && toLevel == 0)
             gameObject.SetActive(false);
@@ -48,15 +68,39 @@ public class MenuWindow : MonoBehaviour {
     {
         fromLevel = 0f;
         toLevel = 1f;
-        currentLevel = 0f;
+        //currentLevel = 0f;
         startTime = Time.time;
         gameObject.SetActive(true);
+        if (!isIndicator)
+        {
+            currentLevel = 0f;
+            foreach (MenuWindow ind in indicatorCallList)
+                ind.OnWindowCallIndicator(this, true);
+        }
     }
     public void FadeOut()
     {
         fromLevel = 1f;
         toLevel = 0f;
-        currentLevel = 1f;
+        //currentLevel = 1f;
         startTime = Time.time;
+        if (!isIndicator)
+        {
+            currentLevel = 1f;
+            foreach (MenuWindow ind in indicatorCallList)
+                ind.OnWindowCallIndicator(this, false);
+        }
+    }
+
+    public void OnWindowCallIndicator(MenuWindow wnd, bool state)
+    {
+        if (state)
+            indicatorCount++;
+        else
+            indicatorCount--;
+        if (indicatorCount == 0)
+            FadeIn();
+        else
+            FadeOut();
     }
 }
