033e23c9d36dfc579bece266b827045592117381
bartosz <bartosz@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-26 22:19:07 +0000

    First robot

---
+++ testAR/Assets/Prefabs/Robot1/DebugClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugClick : MonoBehaviour {
+
+    Animator animat;
+
+	// Use this for initialization
+	void Start () {
+        animat = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animat.SetBool("isFighting", !animat.GetBool("isFighting"));
+        }
+	}
+}
