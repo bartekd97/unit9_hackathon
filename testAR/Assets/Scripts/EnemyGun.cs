31123ecd8de53561bc7162b2b4437d0407123d42
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-27 08:52:23 +0000

    robots fixex

---
+++ testAR/Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+
+    public float shotDuration;
+    public float laserDamage;
+    public float interval;
+
+    private GameObject laserRay;
+    void Start () {
+        laserRay = gameObject.transform.GetChild(0).gameObject;
+        laserRay.SetActive(false);
+        StartCoroutine(Shooting());
+    }
+	
+    IEnumerator Shooting()
+    {
+        while (true)
+        {
+            laserRay.SetActive(true);
+            yield return new WaitForSeconds(shotDuration);
+            laserRay.SetActive(false);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
