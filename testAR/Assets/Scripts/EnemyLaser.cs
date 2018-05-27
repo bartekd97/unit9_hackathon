6f58cf5ed51eb681eaf6a0719090a69058cc859b
aleksander <aleksander@5b9eafda-3d42-43b4-aee5-9311a733c9ed>
2018-05-27 08:52:23 +0000

    robots fixex

---
+++ testAR/Assets/Scripts/EnemyLaser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaser : MonoBehaviour {
+
+    private float damage;
+    private float duration;
+
+    private EnemyGun enemyGunController;
+	void Start () {
+        enemyGunController = gameObject.transform.parent.gameObject.GetComponent<EnemyGun>();
+        damage = enemyGunController.laserDamage;
+        duration = enemyGunController.shotDuration;
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("other in trigger: " + other.gameObject.tag);
+        if (other.gameObject.tag == "BitcoinMinerCollision")
+        {
+            other.gameObject.GetComponentInParent<Health>().SubtractHealth(damage);
+        }
+    }
+
+}
